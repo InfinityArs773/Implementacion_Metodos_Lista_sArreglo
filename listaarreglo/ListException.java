344f6ae2f7e2b73e75fc2f04cfdcc337938570a8
InfinityArs773 <ericklopez3759@gmail.com>
2026-09-24 20:14:24 -0700

    Add files via upload

---
+++ listaarreglo/ListException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaarreglo;
+
+/**
+ *
+ * @author labitson
+ */
+public class ListException extends RuntimeException {
+    
+    public ListException(){
+        super();
+    }
+    
+    public ListException(String msg){
+        super(msg);
+    }
+}
